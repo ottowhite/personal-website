dad804ed2b90594a2c6a97a930e0489e896776ee
White, Otto <otto.white20@imperial.ac.uk>
2025-05-03 22:30:12 +0100

    Add nix flake for development with node

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A minimal development environment for the Trauma Timeline app";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_20
+            nodePackages.npm
+            tailwindcss
+          ];
+          shellHook = ''
+            exec zsh
+          '';
+        };
+      });
+} 
