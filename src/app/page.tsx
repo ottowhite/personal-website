95dc8012ca406a19d45e94f27b236ac710b4f1c5
White, Otto <otto.white20@imperial.ac.uk>
2025-06-07 13:02:31 +0100

    Update main page to eliminate eslint errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex min-h-[calc(100vh-4rem)] flex-col items-center justify-center">
       <h1 className="text-4xl font-bold mb-4">Welcome</h1>
-      <p className="text-xl text-gray-400">I'm Otto White</p>
+      <p className="text-xl text-gray-400">I&apos;m Otto White</p>
     </div>
   );
 }
